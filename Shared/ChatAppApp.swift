7a9271afe2ac0bf02d7398f0ef3777e5a9380497
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2020-09-27 17:36:27 -0700

    Initial Commit

---
+++ Shared/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  Shared
+//
+//  Created by Melissa Zellhuber on 27/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
